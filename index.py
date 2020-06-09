d199b52f36e3c94c1bbea95708fc3ea6e02af051
Amnon Skladman <amnon.skladman@gmail.com>
2020-06-09 08:36:51 -0400

    Added more content to the first chapter and refinement.

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ from src.components import hero
 
 os.system("chcp 65001")
 
-city_list = ["Taipei", "Osaka", "Toronto", "Busan", "Vancouver"]
+city_list = ["Taipei", "Osaka", "Toronto", "Busan", "Vancouver", "Singapore"]
 die = 1
 picked_city = ""
 
@@ -41,7 +41,7 @@ def pick_city():
 
 sleep(3)
 
-print("You open your eyes and find yourself sitting in an old dungeon, the walls wet with moisture and covered in moss. In front of you is an old man in jade-coloured robes, his beard reaching his the center of his chest. You are both seated at a small, square table, one die upon it. The die is facing up, revealing the number %s.\n" % (die))
+print("You open your eyes and find yourself sitting in an old dungeon, the walls wet with moisture and covered in moss. In front of you sits an old man in jade-coloured robes, his beard reaching his chest. You are both seated at a small, square table, one die upon it. The die is facing up, revealing the number %s.\n" % (die))
 sleep(3)
 
 roll_die()
@@ -61,11 +61,11 @@ if (pre_quest_1 == 'y'):
     sleep(3)
 else:
     sleep(1.5)
-    print("'Shame on you and your family,' he says, before punching you in the face for wasting his time. For an old man, he sure can punch quite hard. Your head literally explodes and you die. GAME OVER.")
+    print("'Very well, I suppose you're not one for adventures after all. In that case, I must leave you, but you shall be here forever.' The old man disappears, and the room grows dark. This is your demise. GAME OVER!")
     sleep(3)
     sys.exit()
 
-print("'You may be curious as to why you are here, but I need you to go to %s. The truth is, I need a mercenary for a quick job. An important scroll of mine has been stolen, and I need it back. I do not ask questions about where my mercenaries are sourced from. However, I can tell from your visible confusion that you did not come here voluntarily. If you complete this job successfuly, I may look into buying your freedom. However, I need your complete guarantee that you will not fail this mission or seek your own path until it is complete.'\n" % (picked_city))
+print("'You may be curious as to why you are here. I require your help. An important scroll of mine has been stolen, and I need it back. I can tell from your visible confusion that you did not come here voluntarily, and most likely wish to return home. If you complete this job successfuly, I promise I will help you return home. However, I need your complete guarantee that you will not fail this mission or seek your own path until it is complete. You will need to go to %s.'\n" % (picked_city))
 sleep(3)
 
 player_name = str(input("'Actually, pardon my manners, I have not yet introduced myself. I am Andrew The Grey, and you are?' "))
@@ -73,15 +73,17 @@ player_name = str(input("'Actually, pardon my manners, I have not yet introduced
 print("'I can't say I have ever heard of that name, but if that is your name, then that is your name. As I have said before, %s, you must go and retrieve my scroll. If it falls into the wrong hands, it could mean the end of the world as we know it. I have prepared some gear for you right here. I hope it suffices. Once you are ready, please head out and make your way to the nearest port.'\n" % (player_name))
 sleep(3)
 
-quest_1_1 = input("Andrew The Grey stands up, snaps his fingers, and poofs into thin air. You blink several times, trying to make sense of what you just witnessed. You then look to the side of the table and see a bag. In it, are a bow and quiver, short-sword, and a note saying 'PICK ONLY ONE, DO NOT BE GREEDY.' Which will you pick? [bow, sword] ")
+quest_1_1 = input("Andrew The Grey stands up, snaps his fingers, and poofs into thin air. You blink several times, trying to make sense of what you just witnessed. You then look to the side of the table and see a bag. In it are a bow and quiver, a short-sword, and a note saying 'PICK ONLY ONE, DO NOT BE GREEDY.' Which will you pick? [bow, sword] ")
 
 #TODO: Implement a function here for checking if the user uses the correct stuff
 
 if quest_1_1 == 'bow':
-    print("BOW")
+    sleep(1)
+    print("YOU HAVE PICKED THE BOW AND ARROW!")
 elif quest_1_1 == 'sword':
-    print("SWORD")
+    sleep(1)
+    print("YOU HAVE PICKED THE SHORT SWORD!")
 else:
-    quest_1_1 = input("Please try again - which will you choose? ")
+    quest_1_1 = input("Please try again - which will you choose? [bow, sword] ")
 
 print("You pick your %s up and look around." % (quest_1_1))
