c5ac0f7ef7dd5aab66e3256c6ed46227e5da0cb4
Amnon Skladman <askladman@tpl.ca>
2021-11-07 10:51:49 -0500

    Added spacing, new alt hero.

--- index.py
+++ index.py
@@ -4,10 +4,10 @@ import os
 os.system('') # Allows to use ANSI escape sequences on Windows
 import sys
 from time import sleep # adds ability to "wait"
-from src.components.images.hero import hero # hero banner
+from src.components.images.hero import heroAlt # hero banner
 
 #default values
-city_list = ["Taipei", "Osaka", "Toronto", "Busan", "Vancouver", "Singapore"]
+city_list = ["Taipei", "Osaka", "Toronto", "Busan", "Vancouver", "Singapore", "London", "Montreal", "Paris"]
 die = 1
 picked_city = ""
 inventory = []
@@ -59,15 +59,15 @@ sleep(3)
 pick_city()
 sleep(3)
 
-pre_quest_1 = input("'Would you stay a while and listen?' [y/n] ")
+pre_quest_1 = input("\n'Would you stay a while and listen?' [\u001b[33my\u001b[0m/\u001b[33mn\u001b[0m] ")
 
 if (pre_quest_1 == 'y'):
     sleep(1.5)
-    print("'Thank you. Now sit back for a moment and hear me speak.'\n")
+    print("\n'Thank you. Now sit back for a moment and hear me speak.'\n")
     sleep(3)
 else:
     sleep(1.5)
-    print("'Very well, I suppose you're not one for adventures after all. In that case, best of luck, and don't let the spiders kill you...' The old man disappears, and the room grows dark. This is your demise. \u001b[31mGAME OVER!\u001b[0m")
+    print("\n'Very well, I suppose you're not one for adventures after all. In that case, best of luck, and don't let the spiders kill you...' The old man disappears, and the room grows dark. This is your demise. \u001b[31mGAME OVER!\u001b[0m")
     sleep(3)
     sys.exit() # exits the game if player chooses "n"
 
@@ -76,63 +76,63 @@ sleep(3)
 
 player_name = str(input("'Actually, pardon my manners, I have not yet introduced myself. I am Andrew The Grey, and you are?' "))
 
-print("'I can't say I have ever heard of that name, but if that is your name, then that is your name. As I have said before, \u001b[33m%s\u001b[0m, you must go and retrieve my scroll. If it falls into the wrong hands, it could mean the end of the world as we know it. I have prepared some gear for you right here. I hope it suffices. Once you are ready, please head out and make your way to the nearest port.'\n" % (player_name))
+print("\n'I can't say I have ever heard of that name, but if that is your name, then that is your name. As I have said before, \u001b[33m%s\u001b[0m, you must go and retrieve my scroll. If it falls into the wrong hands, it could mean the end of the world as we know it. I have prepared some gear for you right here. I hope it suffices. Once you are ready, please head out and make your way to the nearest port.'\n" % (player_name))
 sleep(3)
 
 print("Andrew The Grey stands up, snaps his fingers, and poofs into thin air. You blink several times, trying to make sense of what you just witnessed. You then look to the side of the table and see a bag. In it are a bow and quiver, a short-sword, and a note saying 'PICK ONLY ONE, DO NOT BE GREEDY.'")
 
 def quest_1_1_check():
-    quest_1_1 = input("Which will you pick? [\u001b[33mbow\u001b[0m, \u001b[33msword\u001b[0m] ")
+    quest_1_1 = input("\nWhich will you pick? [\u001b[33mbow\u001b[0m, \u001b[33msword\u001b[0m] ")
 
     if quest_1_1 == 'bow':
         sleep(1)
-        print("YOU HAVE PICKED THE \u001b[33mBOW AND ARROW\u001b[0m!")
+        print("\nYOU HAVE PICKED THE \u001b[33mBOW AND ARROW\u001b[0m!")
         weapon.append('bow')
         sleep(1)
     elif quest_1_1 == 'sword':
         sleep(1)
-        print("YOU HAVE PICKED THE \u001b[33mSHORT SWORD\u001b[0m!")
+        print("\nYOU HAVE PICKED THE \u001b[33mSHORT SWORD\u001b[0m!")
         weapon.append('short-sword')
         sleep(1)
     else:
         sleep(1)
-        print("Please try again.")
+        print("\nPlease try again.")
         sleep(1)
         quest_1_1_check()
 
 quest_1_1_check()
 
-print("You pick your \u001b[33m%s\u001b[0m up and look around. As you see it, there are only two real options. You could either inspect the room to see if you could find anything in the moss, or you could leave the room and go upstairs." % (weapon[0]))
+print("\nYou pick your \u001b[33m%s\u001b[0m up and look around. As you see it, there are only two real options. You could either inspect the room to see if you could find anything in the moss, or you could leave the room and go upstairs." % (weapon[0]))
 sleep(1)
 
 def quest_1_2_check():
-    quest_1_2 = input("What will you do? [\u001b[33minspect\u001b[0m, \u001b[33mleave\u001b[0m] ")
+    quest_1_2 = input("\nWhat will you do? [\u001b[33minspect\u001b[0m, \u001b[33mleave\u001b[0m] ")
 
     if quest_1_2 == 'inspect':
         sleep(1)
-        print("You start looking around the room in hopes of finding anything useful. The room is empty, save for the table and two chairs, and perhaps the moss. You walk up to the moss and look at it closely. Something shiny catches your eye. You pick it up. It's a \u001b[33mkey\u001b[0m!")
+        print("\nYou start looking around the room in hopes of finding anything useful. The room is empty, save for the table and two chairs, and perhaps the moss. You walk up to the moss and look at it closely. Something shiny catches your eye. You pick it up. It's a \u001b[33mkey\u001b[0m!")
         inventory.append('key')
         sleep(1)
     elif quest_1_2 == 'leave' and 'key' in inventory:
         sleep(1)
-        print("You grab your weapon and begin to make your way to the stairs. You go up and reach a door. Freedom! It's time to open it and begin your adventure.")
+        print("\nYou grab your weapon and begin to make your way to the stairs. You go up and reach a door. Freedom! It's time to open it and begin your adventure.")
         sleep(1)
     elif quest_1_2 == 'leave':
         sleep(1)
-        print("You grab your weapon and begin to make your way to the stairs. You go up and reach a door. Freedom! You try to open it, but it's locked. The \u001b[33mkey\u001b[0m must still be in the room.")
+        print("\nYou grab your weapon and begin to make your way to the stairs. You go up and reach a door. Freedom! You try to open it, but it's locked. The \u001b[33mkey\u001b[0m must still be in the room.")
         sleep(1)
-        print("You return to the room.")
+        print("\nYou return to the room.")
         sleep(1)
         quest_1_2_check()
     else:
         sleep(1)
-        print("Please try again.")
+        print("\nPlease try again.")
         sleep(1)
         quest_1_2_check()
 
 quest_1_2_check()
 
-print("You slide the key into the door and turn it. The door unlocks with a loud click sound. The \u001b[33mkey\u001b[0m is now stuck in the door and no longer useable. You then open the door and see that you were in the basement of a once-glorious tavern that now lies in ruins. The wind blows gracefully, and all around you are green meadows with majestic mountains standing in the distance.")
+print("\nYou slide the key into the door and turn it. The door unlocks with a loud click sound. The \u001b[33mkey\u001b[0m is now stuck in the door and no longer useable. You then open the door and see that you were in the basement of a once-glorious tavern that now lies in ruins. The wind blows gracefully, and all around you are green meadows with majestic mountains standing in the distance.\n")
 inventory.remove('key')
 sleep(3)
 
