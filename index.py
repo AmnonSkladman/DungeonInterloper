7a399bc3eefd417de69b39c32209a060f1a69d16
Amnon Skladman <askladman@tpl.ca>
2023-04-27 15:33:05 -0400

    Content and formatting updates

--- index.py
+++ index.py
@@ -6,25 +6,25 @@ import sys
 from time import sleep # adds ability to "wait"
 from src.components.images.hero import heroAlt # hero banner
 
-#default values
-city_list = ["Taipei", "Osaka", "Toronto", "Busan", "Vancouver", "Singapore", "London", "Montreal", "Paris"]
+# Default values
+city_list = ["Taipei", "Osaka", "Toronto", "Seoul", "Vancouver", "Amsterdam", "London", "Montreal", "Singapore"]
 die = 1
 picked_city = ""
 inventory = []
 weapon = []
 armour = []
 
-# animates dice by showing a die and replacing it with next one
+# Animates dice by showing a die and replacing it with next one
 def dice_animation():
     dice = [u'⚀',u'⚁',u'⚂',u'⚃',u'⚄',u'⚅']
     for die in dice:
         print(die, end="\r", flush=True)
         sleep(0.1)
 
-# runs dice animation 3 times and prints result from RNG
+# Runs dice animation 3 times and prints result from RNG
 def roll_die():
     global die
-    print("The old man picks the die up and rolls it...")
+    print("The old man picks the die up and rolls it...\n")
     sleep(0.25)
     for _ in range(3):
         dice_animation()
@@ -37,23 +37,24 @@ def roll_die():
         5: '5',
         6: '6'
     }
-    die_value = switcher.get(die, "Uhh, I screwed up... Let's try again!")
-    print("\nHe rolled a \u001b[33m%s\u001b[0m." % (die_value))
+    # Returns the die value, or a default value.
+    die_value = switcher.get(die, "I think my die is broken...")
+    print("\n\nHe rolled a \u001b[33m%s\u001b[0m.\n" % (die_value))
 
-# picks a random city
+# Picks a random city
 def pick_city():
     global picked_city
     picked_city = choice(city_list)
 
 sleep(3)
 
-print("You open your eyes and find yourself sitting in an old dungeon, the walls wet with moisture and covered in moss. In front of you sits an old man in jade-coloured robes, his beard reaching his chest. You are both seated at a small, square table, one die upon it. The die is facing up, revealing the number \u001b[33m%s\u001b[0m.\n" % (die))
+print("You open your eyes and find yourself sitting in what looks to be a dungeon, the walls wet with moisture and covered in moss. You remember being on a plane on your way to see your in-laws, and now you're...here? Where even is \"here\"?\n\nIn front of you sits an old man in jade-coloured robes, his beard reaching his chest. You are both seated at a small, square table, one die upon it. The die is facing up, revealing the number \u001b[33m%s\u001b[0m.\n\nHe smiles at you as he picks the die up, and rolls it gently.\n" % (die))
 sleep(3)
 
 roll_die()
-sleep(1)
+sleep(3)
 
-print("\n'Ah, \u001b[33m%s\u001b[0m, not a bad number. Although it may mean nothing to you right now, it will come back in the near future, either to aid you, or harm you.'" % (die))
+print("'Ah, \u001b[33m%s\u001b[0m, interesting. Although it may mean nothing to you right now, it will make sense very soon, I promise.'" % (die))
 sleep(3)
 
 pick_city()
@@ -63,23 +64,23 @@ pre_quest_1 = input("\n'Would you stay a while and listen?' [\u001b[33my\u001b[0
 
 if (pre_quest_1 == 'y'):
     sleep(1.5)
-    print("\n'Thank you. Now sit back for a moment and hear me speak.'\n")
+    print("\n'Marvelous!'\n")
     sleep(3)
 else:
     sleep(1.5)
-    print("\n'Very well, I suppose you're not one for adventures after all. In that case, best of luck, and don't let the spiders kill you...' The old man disappears, and the room grows dark. This is your demise. \u001b[31mGAME OVER!\u001b[0m")
+    print("\n'Very well, I suppose you'd rather remain here after all. In that case, best of luck, and don't let the spiders kill you...' The old man gets up from his chair and disappears, the room growing dark by the second. This is your demise. \u001b[31mGAME OVER!\u001b[0m")
     sleep(3)
     sys.exit() # exits the game if player chooses "n"
 
-print("'You may be curious as to why you are here. I require your help. An important scroll of mine has been stolen, and I need it back. I can tell from your visible confusion that you did not come here voluntarily, and most likely wish to return home. If you complete this job successfuly, I promise I will help you return home. However, I need your complete guarantee that you will not fail this mission or seek your own path until it is complete. You will need to go to \u001b[33m%s\u001b[0m.'\n" % (picked_city))
+print("'You may be curious as to why you are here. Well, you see, I require your help. An important scroll of mine has been stolen from my library, and I need it back. I can tell from your visible confusion that you did not come here voluntarily, and most likely wish to return home. If you help me, I promise I will help you in return. If you agree, then you will need to go to \u001b[33m%s\u001b[0m.'\n" % (picked_city))
 sleep(3)
 
-player_name = str(input("'Actually, pardon my manners, I have not yet introduced myself. I am Andrew The Grey, and you are?' "))
+player_name = str(input("'Actually, pardon my manners, I have not even introduced myself yet! I am Andrew The Grey, and you are?' "))
 
-print("\n'I can't say I have ever heard of that name, but if that is your name, then that is your name. As I have said before, \u001b[33m%s\u001b[0m, you must go and retrieve my scroll. If it falls into the wrong hands, it could mean the end of the world as we know it. I have prepared some gear for you right here. I hope it suffices. Once you are ready, please head out and make your way to the nearest port.'\n" % (player_name))
+print("\n'I can't say I have ever heard of that name, but it's a pleasure to meet your acquaintance. As I have said before, \u001b[33m%s\u001b[0m, you must go and retrieve my scroll. If it falls into the wrong hands, it would be most awful for everyone. I have prepared some gear for you right here. Once you are ready, please come and meet me outside.'\n" % (player_name))
 sleep(3)
 
-print("Andrew The Grey stands up, snaps his fingers, and poofs into thin air. You blink several times, trying to make sense of what you just witnessed. You then look to the side of the table and see a bag. In it are a bow and quiver, a short-sword, and a note saying 'PICK ONLY ONE, DO NOT BE GREEDY.'")
+print("Andrew The Grey stands up, snaps his fingers, and poofs into thin air. You blink several times, trying to make sense of what you just witnessed. You then look to the side of the table and see a bag. In it are a bow and quiver, a short-sword, and a note saying 'PICK ONLY ONE.'")
 
 def quest_1_1_check():
     quest_1_1 = input("\nWhich will you pick? [\u001b[33mbow\u001b[0m, \u001b[33msword\u001b[0m] ")
@@ -132,7 +133,7 @@ def quest_1_2_check():
 
 quest_1_2_check()
 
-print("\nYou slide the key into the door and turn it. The door unlocks with a loud click sound. The \u001b[33mkey\u001b[0m is now stuck in the door and no longer useable. You then open the door and see that you were in the basement of a once-glorious tavern that now lies in ruins. The wind blows gracefully, and all around you are green meadows with majestic mountains standing in the distance.\n")
+print("\nYou slide the key into the door and turn it. The door unlocks with a loud click sound. The \u001b[33mkey\u001b[0m is now stuck in the door and no longer useable. You then open the door and see that you were in the basement of a once-glorious tavern that now lies in ruins. The wind blows gracefully, and all around you are green meadows with majestic mountains at the edge of the horizon.\n\nIn the distance, you see Andrew The Grey sitting on a boulder, waiting for you. This is where your adventure begins.\n")
 inventory.remove('key')
 sleep(3)
 
